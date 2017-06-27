048c714aae4ac4f70161cc18b6777d47fc7cb050
Yun Zhou <yun.zhou@simplyenergy.com.au>
2017-06-27 15:56:06 +1000

    crack code book python implementation

---
+++ count_inversion.py
@@ -0,0 +1,42 @@
+def count_inversions(alist):
+    #print("Splitting ",alist)
+    if len(alist)>1:
+        mid = len(alist)//2
+        lefthalf = alist[:mid]
+        righthalf = alist[mid:]
+
+        count_left = count_inversions(lefthalf)
+        count_right = count_inversions(righthalf)
+        count_merge = 0
+        i=0
+        j=0
+        k=0
+        while i < len(lefthalf) and j < len(righthalf):
+            if lefthalf[i] <= righthalf[j]:
+                alist[k]=lefthalf[i]
+                i=i+1
+            else:
+                count_merge += mid - i
+                alist[k]=righthalf[j]
+                j=j+1
+            k=k+1
+
+        while i < len(lefthalf):
+            alist[k]=lefthalf[i]
+            i=i+1
+            k=k+1
+
+        while j < len(righthalf):
+            alist[k]=righthalf[j]
+            j=j+1
+            k=k+1
+        return count_merge+count_left+count_right
+    else:
+        return 0
+
+
+t = int(input().strip())
+for a0 in range(t):
+    n = int(input().strip())
+    arr = list(map(int, input().strip().split(' ')))
+    print(count_inversions(arr))
